9683412284a56d8a4daf59f2ff7392b48b31ad0e
hyejungg <kimhyejung12@naver.com>
2022-08-25 22:55:55 +0900

    #9 [fix] 온보딩 정보 등록 여부 조회 api return 200 -> 204 로 변경

--- src/main/java/hous/server/common/success/SuccessCode.java
+++ src/main/java/hous/server/common/success/SuccessCode.java
@@ -1,11 +1,12 @@
 package hous.server.common.success;
 
+import static hous.server.common.success.SuccessStatusCode.CREATED;
+import static hous.server.common.success.SuccessStatusCode.NO_CONTENT;
+import static hous.server.common.success.SuccessStatusCode.OK;
+
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-
-import static hous.server.common.success.SuccessStatusCode.CREATED;
-import static hous.server.common.success.SuccessStatusCode.OK;
 
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
@@ -14,16 +15,13 @@ public enum SuccessCode {
     /**
      * 200 OK
      */
-    SUCCESS(OK, "성공입니다."),
-
     // 인증
     LOGIN_SUCCESS(OK, "로그인 성공입니다."),
     REISSUE_TOKEN_SUCCESS(OK, "토큰 갱신 성공입니다."),
 
     // 온보딩
     CHECK_ONBOARDING_SUCCESS(OK, "온보딩 등록여부 조회 성공입니다."),
-    SET_ONBOARDING_SUCCESS(OK, "온보딩 정보 등록 성공입니다."),
-    
+
     // 방
     GET_ROOM_SUCCESS(OK, "참가중인 방 조회 성공입니다."),
     JOIN_ROOM_SUCCESS(OK, "방 참여 성공입니다."),
@@ -31,7 +29,7 @@ public enum SuccessCode {
     /**
      * 201 CREATED
      */
-     
+
     // 방
     CREATE_ROOM_SUCCESS(CREATED, "방 생성 성공입니다."),
 
@@ -42,7 +40,7 @@ public enum SuccessCode {
     /**
      * 204 NO_CONTENT
      */
-    ;
+    NO_CONTENT_SUCCESS(NO_CONTENT, "");
 
     private final SuccessStatusCode statusCode;
     private final String message;
