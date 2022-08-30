7f93b8cb7c1923ba3bc07b21a40581ca29c9a91f
hyejungg <kimhyejung12@naver.com>
2022-08-30 14:29:33 +0900

    #15 [fix] 형태 통일

--- src/main/java/hous/server/service/user/dto/request/SetOnboardingInfoRequestDto.java
+++ src/main/java/hous/server/service/user/dto/request/SetOnboardingInfoRequestDto.java
@@ -2,15 +2,14 @@ package hous.server.service.user.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModelProperty;
+import java.time.LocalDate;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import org.hibernate.validator.constraints.Length;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import java.time.LocalDate;
 
 @ToString
 @Getter
@@ -19,7 +18,7 @@ public class SetOnboardingInfoRequestDto {
 
     @ApiModelProperty(value = "닉네임", example = "혜조니")
     @NotBlank(message = "{onboarding.nickname.notBlank}")
-    @Length(min = 1, max = 5, message = "{onboarding.nickname.length}")
+    @Size(min = 1, max = 5, message = "{onboarding.nickname.length}")
     private String nickname;
 
     @ApiModelProperty(value = "생년월일", example = "1999-03-04")
