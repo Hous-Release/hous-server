1ab749800e37af32ccf445dde08342666bf20d75
HyeokJoon Kong <orijoon98@gmail.com>
2022-09-02 21:29:28 +0900

    #39 [fix] 요일 표시 수정에 따른 response dto 수정

--- src/main/java/hous/server/domain/todo/DayOfWeek.java
+++ src/main/java/hous/server/domain/todo/DayOfWeek.java
@@ -1,11 +1,43 @@
 package hous.server.domain.todo;
 
-public enum DayOfWeek {
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY,
-    SUNDAY;
+import hous.server.common.model.EnumModel;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Comparator;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public enum DayOfWeek implements EnumModel {
+    MONDAY("월", 1),
+    TUESDAY("화", 2),
+    WEDNESDAY("수", 3),
+    THURSDAY("목", 4),
+    FRIDAY("금", 5),
+    SATURDAY("토", 6),
+    SUNDAY("일", 7);
+
+    private final String value;
+
+    private final int index;
+
+    @Override
+    public String getKey() {
+        return name();
+    }
+
+    @Override
+    public String getValue() {
+        return value;
+    }
+
+    public static String toString(Set<DayOfWeek> dayOfWeekSet) {
+        return dayOfWeekSet.stream()
+                .sorted(Comparator.comparing(DayOfWeek::getIndex))
+                .map(DayOfWeek::getValue)
+                .collect(Collectors.joining(", "));
+    }
 }
--- src/main/java/hous/server/service/todo/dto/response/TodoSummaryInfoResponse.java
+++ src/main/java/hous/server/service/todo/dto/response/TodoSummaryInfoResponse.java
@@ -5,7 +5,6 @@ import hous.server.domain.todo.Todo;
 import hous.server.domain.user.Onboarding;
 import lombok.*;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -19,7 +18,7 @@ public class TodoSummaryInfoResponse {
 
     private String name;
     private List<UserPersonalityInfo> selectedUsers;
-    private List<DayOfWeek> dayOfWeeks;
+    private String dayOfWeeks;
 
     public static TodoSummaryInfoResponse of(Todo todo, List<UserPersonalityInfo> userPersonalityInfos, Onboarding me) {
         Set<DayOfWeek> dayOfWeekSet = new HashSet<>();
@@ -27,7 +26,7 @@ public class TodoSummaryInfoResponse {
         return TodoSummaryInfoResponse.builder()
                 .name(todo.getName())
                 .selectedUsers(UserPersonalityInfo.sortMeFirst(userPersonalityInfos, me))
-                .dayOfWeeks(new ArrayList<>(dayOfWeekSet))
+                .dayOfWeeks(DayOfWeek.toString(dayOfWeekSet))
                 .build();
     }
 }
