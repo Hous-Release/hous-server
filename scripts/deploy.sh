8075de37868993716e8ac78fdde137afdbf5e333
hyejungg <kimhyejung12@naver.com>
2022-09-02 22:13:16 +0900

    #38 [fix] deploy 시 SIGTERM 신호 받을 경우, 실행하도록 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,16 +10,15 @@ cp $BUILD_JAR $DEPLOY_PATH
 echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/hous/deploy.log
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
+DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
+
 if [ -z $CURRENT_PID ]
 then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/hous/deploy.log
+  echo "> DEPLOY_JAR 배포" >> /home/ubuntu/hous/deploy.log
+  nohup java -jar $DEPLOY_JAR >> /home/ubuntu/hous/deploy.log 2>/home/ubuntu/hous/deploy_err.log &
 else
+  trap echo "> DEPLOY_JAR 배포" >> /home/ubuntu/hous/deploy.log | nohup java -jar $DEPLOY_JAR >> /home/ubuntu/hous/deploy.log 2>/home/ubuntu/hous/deploy_err.log & SIGTERM
   echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
-  sleep 15
 fi
-
-DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-
-echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/hous/deploy.log
-nohup java -jar $DEPLOY_JAR >> /home/ubuntu/hous/deploy.log 2>/home/ubuntu/hous/deploy_err.log &
