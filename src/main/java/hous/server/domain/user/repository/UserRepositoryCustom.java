11f5d3f3604ca911fc51ced0f7d01564261a38da
Joon <orijoon98@gmail.com>
2022-09-16 05:01:29 +0900

    #96 [feat] todo 완료 관련 배지 로직 구현

--- src/main/java/hous/server/domain/user/repository/UserRepositoryCustom.java
+++ src/main/java/hous/server/domain/user/repository/UserRepositoryCustom.java
@@ -3,6 +3,8 @@ package hous.server.domain.user.repository;
 import hous.server.domain.user.User;
 import hous.server.domain.user.UserSocialType;
 
+import java.util.List;
+
 public interface UserRepositoryCustom {
 
     boolean existsBySocialIdAndSocialType(String socialId, UserSocialType socialType);
@@ -12,4 +14,6 @@ public interface UserRepositoryCustom {
     User findUserBySocialIdAndSocialType(String socialId, UserSocialType socialType);
 
     User findUserByFcmToken(String fcmToken);
+
+    List<User> findAllUsers();
 }
