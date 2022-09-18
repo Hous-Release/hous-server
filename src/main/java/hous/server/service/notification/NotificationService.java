e327f06db71e80b0901e814a7e1e4f5f7dd44019
HyeokJoon Kong <orijoon98@gmail.com>
2022-09-18 17:51:25 +0900

    #102 [fix] 푸시 알림 보낼 때 알림 설정 반영

--- src/main/java/hous/server/service/notification/NotificationService.java
+++ src/main/java/hous/server/service/notification/NotificationService.java
@@ -8,6 +8,7 @@ import hous.server.domain.notification.PushMessage;
 import hous.server.domain.notification.repository.NotificationRepository;
 import hous.server.domain.rule.Rule;
 import hous.server.domain.user.Onboarding;
+import hous.server.domain.user.PushStatus;
 import hous.server.domain.user.User;
 import hous.server.service.firebase.FirebaseCloudMessageService;
 import lombok.RequiredArgsConstructor;
@@ -24,12 +25,16 @@ public class NotificationService {
 
     public void sendNewRuleNotification(User to, Rule rule) {
         notificationRepository.save(Notification.newInstance(to.getOnboarding(), NotificationType.RULE, newRuleNotification(rule), false));
-        firebaseCloudMessageService.sendMessageTo(to.getFcmToken(), PushMessage.NEW_RULE.getTitle(), PushMessage.NEW_RULE.getBody());
+        if (to.getSetting().isPushNotification() && to.getSetting().getRulesPushStatus() == PushStatus.ON) {
+            firebaseCloudMessageService.sendMessageTo(to.getFcmToken(), PushMessage.NEW_RULE.getTitle(), PushMessage.NEW_RULE.getBody());
+        }
     }
 
     public void sendNewBadgeNotification(User to, BadgeInfo badgeInfo) {
         notificationRepository.save(Notification.newInstance(to.getOnboarding(), NotificationType.BADGE, newBadgeNotification(badgeInfo), false));
-        firebaseCloudMessageService.sendMessageTo(to.getFcmToken(), newBadgePushTitle(badgeInfo), newBadgePushBody(to.getOnboarding()));
+        if (to.getSetting().isPushNotification() && to.getSetting().getBadgePushStatus() == PushStatus.ON) {
+            firebaseCloudMessageService.sendMessageTo(to.getFcmToken(), newBadgePushTitle(badgeInfo), newBadgePushBody(to.getOnboarding()));
+        }
     }
 
     private String newRuleNotification(Rule rule) {
