97e9f2b897cafc63b42fd834bec8fe1edd6968e3
hyejungg <kimhyejung12@naver.com>
2022-09-22 15:47:06 +0900

    #122 [feat] 호미의 탄생 배지 구현

--- src/main/java/hous/server/domain/user/repository/UserRepositoryImpl.java
+++ src/main/java/hous/server/domain/user/repository/UserRepositoryImpl.java
@@ -6,6 +6,7 @@ import hous.server.domain.user.UserSocialType;
 import hous.server.domain.user.UserStatus;
 import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import static hous.server.domain.user.QUser.user;
@@ -65,6 +66,17 @@ public class UserRepositoryImpl implements UserRepositoryCustom {
         return queryFactory
                 .selectFrom(user)
                 .where(user.status.eq(UserStatus.ACTIVE))
+                .fetch();
+    }
+
+    @Override
+    public List<User> findAllUserByBirthday(LocalDate birthday) {
+        return queryFactory
+                .selectFrom(user)
+                .where(
+                        user.status.eq(UserStatus.ACTIVE),
+                        user.onboarding.birthday.eq(birthday)
+                )
                 .fetch();
     }
 }
