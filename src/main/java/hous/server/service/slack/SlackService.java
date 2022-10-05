4b2314a79f5e0d1ce1b15ef5c7aa228bebcfe6a2
hyejungg <kimhyejung12@naver.com>
2022-10-05 15:05:23 +0900

    #128 [feat] attachment 추가

--- src/main/java/hous/server/service/slack/SlackService.java
+++ src/main/java/hous/server/service/slack/SlackService.java
@@ -2,6 +2,7 @@ package hous.server.service.slack;
 
 import com.slack.api.Slack;
 import com.slack.api.methods.SlackApiException;
+import com.slack.api.model.Attachment;
 import com.slack.api.model.block.LayoutBlock;
 import com.slack.api.model.block.composition.TextObject;
 import hous.server.common.util.YamlPropertySourceFactory;
@@ -28,7 +29,7 @@ public class SlackService {
     String channel;
 
     private static final String SLACK_MESSAGE_TITLE = "🤯 *500 에러 발생*";
-    private static final String SLACK_NOTI_MESSAGE_TITLE = "호엥 에러 발생. 서버 다 모 여";
+    private static final String ATTACHMENTS_COLOR = "#eb4034";
     private static final String FILTER_STRING = "hous.server";
     private static final String SLACK_ERROR_MESSAGE = "*Error Message:*\n";
     private static final String SLACK_ERROR_STACK = "*Error Stack:*\n";
@@ -36,21 +37,30 @@ public class SlackService {
     public void sendSlackMessage(Exception exception) {
         try {
             Slack slack = Slack.getInstance();
-            List<LayoutBlock> layoutBlockList = createSlackMessage(exception);
+            List<Attachment> attachments = createSlackAttachment(exception);
             slack.methods(token).chatPostMessage(req ->
                     req.channel(channel)
-                            .blocks(layoutBlockList)
-                            .text(SLACK_NOTI_MESSAGE_TITLE));
+                            .attachments(attachments)
+                            .text(SLACK_MESSAGE_TITLE));
         } catch (SlackApiException | IOException e) {
             log.error(e.getMessage());
         }
     }
 
+    private List<Attachment> createSlackAttachment(Exception exception) {
+        List<Attachment> attachments = new ArrayList<>();
+        Attachment attachment = new Attachment();
+        attachment.setColor(ATTACHMENTS_COLOR);
+        attachment.setBlocks(createSlackMessage(exception));
+        attachments.add(attachment);
+        return attachments;
+    }
+
+
     private List<LayoutBlock> createSlackMessage(Exception exception) {
         StackTraceElement[] stacks = exception.getStackTrace();
 
         List<LayoutBlock> layoutBlockList = new ArrayList<>();
-        layoutBlockList.add(section(header -> header.text(markdownText(SLACK_MESSAGE_TITLE))));
 
         List<TextObject> sectionInFields = new ArrayList<>();
         sectionInFields.add(markdownText(SLACK_ERROR_MESSAGE + exception.getMessage()));
