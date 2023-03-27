702917b60aa3ea2f88cd5ecdc71c383002a8df52
HyeokJoon Kong <orijoon98@gmail.com>
2023-03-27 18:40:39 +0900

    #306 [test] NotificationRetrieveService 관련 테스트 추가

---
+++ src/test/java/hous/server/service/notification/NotificationRetrieveServiceTest.java
@@ -0,0 +1,64 @@
+package hous.server.service.notification;
+
+import hous.server.domain.room.Room;
+import hous.server.domain.todo.Todo;
+import hous.server.domain.user.User;
+import hous.server.domain.user.UserSocialType;
+import hous.server.domain.user.mysql.UserRepository;
+import hous.server.service.notification.dto.response.NotificationsInfoResponse;
+import hous.server.service.user.UserService;
+import hous.server.service.user.dto.request.CreateUserRequestDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@ActiveProfiles(value = "local")
+@Transactional
+public class NotificationRetrieveServiceTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private NotificationService notificationService;
+
+    @Autowired
+    private NotificationRetrieveService notificationRetrieveService;
+
+    @Test
+    @DisplayName("스크롤 페이지네이션으로 알림 목록 조회 성공")
+    public void get_notifications_info_by_scroll_pagination_success() {
+        // given
+        CreateUserRequestDto createUserRequestDto = CreateUserRequestDto.of(
+                "socialId1", UserSocialType.KAKAO, "fcmToken1", "nickname1", "2022-01-01", true);
+        Long userId = userService.registerUser(createUserRequestDto);
+        User user = userRepository.findUserById(userId);
+        Room room = Room.newInstance(user.getOnboarding(), "room1", "code1");
+        Todo todo = Todo.newInstance(room, "todo1", true);
+        notificationService.sendNewTodoNotification(user, todo, true);
+        notificationService.sendNewTodoNotification(user, todo, true);
+        notificationService.sendNewTodoNotification(user, todo, true);
+
+        // when
+        NotificationsInfoResponse response1 = notificationRetrieveService.getNotificationsInfo(1, Long.MAX_VALUE, userId);
+        NotificationsInfoResponse response2 = notificationRetrieveService.getNotificationsInfo(1, response1.getNextCursor(), userId);
+        NotificationsInfoResponse response3 = notificationRetrieveService.getNotificationsInfo(1, response2.getNextCursor(), userId);
+
+        // then
+        assertThat(response1.getContents()).hasSize(1);
+        assertThat(response1.getNextCursor()).isEqualTo(3);
+        assertThat(response2.getContents()).hasSize(1);
+        assertThat(response2.getNextCursor()).isEqualTo(2);
+        assertThat(response3.getContents()).hasSize(1);
+        assertThat(response3.getNextCursor()).isEqualTo(-1);
+    }
+}
