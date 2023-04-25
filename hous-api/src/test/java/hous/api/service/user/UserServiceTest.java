a7cb09ef3819822853bfeb96c892c2ce7d77d871
hyejungg <kimhyejung12@naver.com>
2023-04-25 21:25:24 +0900

    #319 [feat] 테스트 코드에 컨벤션 적용

--- hous-api/src/test/java/hous/api/service/user/UserServiceTest.java
+++ hous-api/src/test/java/hous/api/service/user/UserServiceTest.java
@@ -1,5 +1,16 @@
 package hous.api.service.user;
 
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
 import hous.api.service.user.dto.request.CreateUserRequestDto;
 import hous.api.service.user.dto.request.DeleteUserRequestDto;
 import hous.core.domain.feedback.FeedbackType;
@@ -8,48 +19,38 @@ import hous.core.domain.room.mysql.RoomRepository;
 import hous.core.domain.user.User;
 import hous.core.domain.user.UserSocialType;
 import hous.core.domain.user.mysql.UserRepository;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ActiveProfiles(value = "local")
 @Transactional
 public class UserServiceTest {
 
-    @Autowired
-    private UserRepository userRepository;
+	@Autowired
+	private UserRepository userRepository;
 
-    @Autowired
-    private RoomRepository roomRepository;
+	@Autowired
+	private RoomRepository roomRepository;
 
-    @Autowired
-    private UserService userService;
+	@Autowired
+	private UserService userService;
 
-    // 정책상 방에 들어가지 않을 경우에만 사용자 삭제가 가능
-    @Test
-    @DisplayName("방에 들어가지 않은 사용자 삭제 성공")
-    public void delete_user_by_not_join_room_success() {
-        // given
-        CreateUserRequestDto createUserRequestDto = CreateUserRequestDto.of(
-                "socialId1", UserSocialType.KAKAO, "fcmToken1", "nickname1", "2022-01-01", true);
-        Long userId = userService.registerUser(createUserRequestDto);
-        DeleteUserRequestDto deleteUserRequestDto = DeleteUserRequestDto.of(FeedbackType.NO, "");
+	// 정책상 방에 들어가지 않을 경우에만 사용자 삭제가 가능
+	@Test
+	@DisplayName("방에 들어가지 않은 사용자 삭제 성공")
+	public void delete_user_by_not_join_room_success() {
+		// given
+		CreateUserRequestDto createUserRequestDto = CreateUserRequestDto.of(
+			"socialId1", UserSocialType.KAKAO, "fcmToken1", "nickname1", "2022-01-01", true);
+		Long userId = userService.registerUser(createUserRequestDto);
+		DeleteUserRequestDto deleteUserRequestDto = DeleteUserRequestDto.of(FeedbackType.NO, "");
 
-        // when
-        userService.deleteUser(deleteUserRequestDto, userId);
+		// when
+		userService.deleteUser(deleteUserRequestDto, userId);
 
-        // then
-        List<User> users = userRepository.findAll();
-        assertThat(users).isEmpty();
-        List<Room> rooms = roomRepository.findAll();
-        assertThat(rooms).isEmpty();
-    }
+		// then
+		List<User> users = userRepository.findAll();
+		assertThat(users).isEmpty();
+		List<Room> rooms = roomRepository.findAll();
+		assertThat(rooms).isEmpty();
+	}
 }
