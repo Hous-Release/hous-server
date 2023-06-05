7c553473be3f8d89116f372cd8d96f7b977e88df
hyejungg <kimhyejung12@naver.com>
2023-06-06 00:35:36 +0900

    #333 [docs] deprecated 될 클래스 주석 추가

--- hous-api/src/main/java/hous/api/service/rule/dto/request/CreateRuleRequestDto.java
+++ hous-api/src/main/java/hous/api/service/rule/dto/request/CreateRuleRequestDto.java
@@ -14,6 +14,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+// TODO Deprecated
 @ToString
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
--- hous-api/src/main/java/hous/api/service/rule/dto/request/UpdateRuleRequestDto.java
+++ hous-api/src/main/java/hous/api/service/rule/dto/request/UpdateRuleRequestDto.java
@@ -13,6 +13,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+// TODO Deprecated
 @ToString
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
