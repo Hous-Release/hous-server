58adeaf6f5265c77a85e2d87e57186917abbc79c
hyejungg <kimhyejung12@naver.com>
2023-06-06 00:33:58 +0900

    #333 [fix] s3 객체 업로드 후 결과 값으로 originalFile, s3 url 받도록 dto 추가

--- hous-api/src/main/java/hous/api/service/image/provider/S3Provider.java
+++ hous-api/src/main/java/hous/api/service/image/provider/S3Provider.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import hous.api.service.image.client.S3FileStorageClient;
+import hous.api.service.image.dto.UploadResponseDto;
 import hous.api.service.image.provider.dto.request.UploadFileRequest;
 import hous.common.exception.ValidationException;
 import lombok.RequiredArgsConstructor;
@@ -18,12 +19,13 @@ public class S3Provider {
 
 	private final S3FileStorageClient fileStorageClient;
 
-	public String uploadFile(UploadFileRequest request, MultipartFile file) {
+	public UploadResponseDto uploadFile(UploadFileRequest request, MultipartFile file) {
 		request.validateAvailableContentType(file.getContentType());
 		String fileName = request.getFileNameWithBucketDirectory(file.getOriginalFilename());
+		String originalFileName = request.getCreateOriginalFileNameWithDate(file.getOriginalFilename());
 		validateImageMaxSize(file);
 		fileStorageClient.uploadFile(file, fileName);
-		return fileStorageClient.getFileUrl(fileName);
+		return UploadResponseDto.of(originalFileName, fileStorageClient.getFileUrl(fileName));
 	}
 
 	public void deleteFile(String fileName) {
