2b713d1b2da2666ca44d428b05847ac99880e58e
orijoon98 <orijoon98@gmail.com>
2023-09-08 01:21:11 +0900

    #354 [fix] 대표 규칙 설정 request 빈 배열 들어올 수 있도록 수정

--- hous-api/src/main/java/hous/api/service/rule/dto/request/UpdateRuleRepresentRequestDto.java
+++ hous-api/src/main/java/hous/api/service/rule/dto/request/UpdateRuleRepresentRequestDto.java
@@ -3,9 +3,7 @@ package hous.api.service.rule.dto.request;
 import java.util.List;
 
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
-import hous.common.constant.Constraint;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -17,6 +15,5 @@ public class UpdateRuleRepresentRequestDto {
 
 	@ApiModelProperty(value = "설정한 대표 규칙 id 리스트", example = "[1, 2, 3]")
 	@NotNull(message = "{rule.list.notNull}")
-	@Size(min = Constraint.RULE_LIST_MIN, message = "{rule.list.min}")
 	private List<Long> rules;
 }
